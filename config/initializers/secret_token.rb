cf0fb640100f1cc78be14d082ec66c1ed12bf3ad
Mike Hunwick <mshunwic@me.com>
2014-09-17 11:01:15 -0600

    initialize myRailOne

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyRailOne::Application.config.secret_key_base = '5672813de6fc3e9e4a056e9b5ab514a9899430649ec89f15929def3b7ad3b4b1fe58734d5700523e150e94ae463dc4237a996459be105baa7356f3e5c47daf07'
